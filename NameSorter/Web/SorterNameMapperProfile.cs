9c6396cd7cfd56233a89a60c592ffcf662fe5043
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2022-01-29 16:07:58 +0700

    setup AutoMapper

---
+++ NameSorter/Web/SorterNameMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+using Web.ViewModels;
+
+namespace Web
+{
+    public class SorterNameMapperProfile:Profile
+    {
+        public SorterNameMapperProfile()
+        {
+            CreateMap<SorterName, SorterNameViewModel>().ReverseMap();
+            CreateMap<SorterNameViewModel, SorterName>().ReverseMap();
+        }
+    }
+}
